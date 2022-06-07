c77fe43493368674fabef13a2c372a244dca5477
Lavon <lavon_c@outlook.com>
2022-06-06 22:24:22 -0400

    commit

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express=require('express');
 const path=require('path');
+const bodyparser = require("body-parser");
 const app=express();
+const session = require("expression-session");
+const {v4:uuidv4} = require("uuid")
 
 
 const port=process.env.Port||3000;
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extented:true}))
 
 app.set('view engine','ejs');
 
@@ -11,13 +17,18 @@ app.set('view engine','ejs');
 app.use('/static',express.static(path.join(__dirname,'public')))
 app.use('/assets',express.static(path.join(__dirname,'public/assets')))
 
-//home route//
+app.use(session({
+    secret:'secret',
+    resave: false,
+    saveUninitialized: true
+}));
 
+//home route//
 app.get('/',(req,res)=> {
     res.render('base',{title:"Login System"})
 })
 
-app.listen(port, ()=>{console.log("listening to the server on http//localhost:3000")})
+app.listen(port, ()=>{console.log("lostening to the server on http://localhost:3000")})
 
 
 
